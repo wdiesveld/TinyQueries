e9a042fff426655af5dcb87ef54753eb549c7c40
Wouter Diesveld <wouter@querytechnology.com>
2015-08-10 15:43:17 +0200

    Removed mb-function; instead added requirement in composer.json

--- libs/Api/HttpTools.class.php
+++ libs/Api/HttpTools.class.php
@@ -2,36 +2,6 @@
 namespace TinyQueries;
 
 /**
- * Add multi-byte function in case mbstring extension is not installed
- *
- */
-if (!function_exists('mb_detect_encoding')) 
-{ 
-	function mb_detect_encoding ($string, $enc=null, $ret=null) 
-	{ 
-        static $enclist = array( 
-            'UTF-8', 'ASCII', 
-            'ISO-8859-1', 'ISO-8859-2', 'ISO-8859-3', 'ISO-8859-4', 'ISO-8859-5', 
-            'ISO-8859-6', 'ISO-8859-7', 'ISO-8859-8', 'ISO-8859-9', 'ISO-8859-10', 
-            'ISO-8859-13', 'ISO-8859-14', 'ISO-8859-15', 'ISO-8859-16', 
-            'Windows-1251', 'Windows-1252', 'Windows-1254', 
-            );
-        
-        $result = false; 
-        
-        foreach ($enclist as $item) { 
-            $sample = iconv($item, $item, $string); 
-            if (md5($sample) == md5($string)) { 
-                if ($ret === NULL) { $result = $item; } else { $result = true; } 
-                break; 
-            }
-        }
-        
-		return $result; 
-	}
-}
-
-/**
  * HttpTools
  *
  * @author 	Wouter Diesveld <wouter@tinyqueries.com>
