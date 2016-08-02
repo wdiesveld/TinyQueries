51516bb629f600a9baa2c3f2a25a427481f865c3
Wouter Diesveld <wouter@tinyqueries.com>
2016-08-02 14:30:23 +0200

    Added download script for downloading queries

---
+++ bin/download-queries.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Download script for downloading the source and compiled queries from tinyqueries.com
+ *
+ */
+
+require_once( dirname(__FILE__) . '/../libs/Compiler.php' );
+
+try
+{
+	$configFile = (count($argv) > 1)
+		? $argv[1]
+		: null;
+	
+	$compiler = new TinyQueries\Compiler( $configFile );
+	
+	$compiler->download();
+}
+catch (Exception $e)
+{
+	echo $e->getMessage() . "\n";
+	exit(1);
+}
+
+exit(0);
